77fbf13678bfd1e536496f84bdc8f5bd1ae75e51
Lea1dev <125105950+Lea1dev@users.noreply.github.com>
2023-03-31 18:20:20 -0300

    Uploaded files

---
+++ script.js
@@ -0,0 +1,9 @@
+// Ao clicar no botão, a fonte, o tamanho da fonte e a cor do texto do parágrafo devem ser
+// alterados.
+var texto = document.getElementById("text");
+
+function js_style() {
+  texto.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif";
+  texto.style.fontSize = "40px";
+  texto.style.color = "orange";
+}
